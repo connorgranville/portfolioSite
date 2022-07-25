725b08d6a3c17257fabdb200e46467b801213510
connorgranville <Connor.granville@gmail.com>
2022-07-25 16:55:41 -0400

    Final Fix?

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-let aboutMe = document.getElementById("about-me");
+let aboutMe = document.getElementById("about-me-mobile");
+let aboutMeText = "ABOUT ME && CONTACT";
 let footer = document.getElementById("footer");
 
+aboutMe.innerHTML = aboutMeText;
 aboutMe.addEventListener("click", expand);
 
 function expand() {
     footer.classList.toggle("expand");
+
+    //doesn't work yet -- changes text
+    if(aboutMe.innerHTML === aboutMeText) {
+        aboutMe.innerHTML = "VV CLOSE ME VV";
+    } else {
+        aboutMe.innerHTML = aboutMeText;
+    }
 }
 

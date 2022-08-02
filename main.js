3cdbacd041feb45728c80a9b2814f562dce4ff66
connorgranville <Connor.granville@gmail.com>
2022-08-02 17:14:56 -0400

    added minor css & JS

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-let aboutMe = document.getElementById("about-me-mobile");
+let footerTitle = document.getElementById("footer-title");
 let aboutMeText = "ABOUT ME && CONTACT";
+let footerText = "website created && maintained by Connor Granville";
 let footer = document.getElementById("footer");
+let widthText = document.getElementById("width");
 
-aboutMe.innerHTML = aboutMeText;
-aboutMe.addEventListener("click", expand);
-
-function expand() {
+const expand = () => {
     footer.classList.toggle("expand");
-
-    //doesn't work yet -- changes text
-    if(aboutMe.innerHTML === aboutMeText) {
-        aboutMe.innerHTML = "VV CLOSE ME VV";
-    } else {
-        aboutMe.innerHTML = aboutMeText;
-    }
 }
 
+const resize = () => {
+    widthText.innerHTML = window.innerWidth;
+
+    if (window.innerWidth > 550) {
+        footerTitle.removeEventListener("click", expand);
+    } else {
+        footerTitle.addEventListener("click", expand);
+    }
+
+    if (window.innerWidth > 550) {
+        footerTitle.innerHTML = footerText;
+    } else {
+        footerTitle.innerHTML = aboutMeText;
+    }
+};
+
+if (window.innerWidth > 550) {
+    footerTitle.innerHTML = footerText;
+} else {
+    footerTitle.innerHTML = aboutMeText;
+};
+
+widthText.innerHTML = window.innerWidth;
+footerTitle.addEventListener("click", expand);
+window.addEventListener("resize", resize);
